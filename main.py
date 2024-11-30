c0a3f92e6803b8661e10c02c753bb2c38593e378
root <root@DESKTOP-LRFIKQA>
2024-11-30 09:54:29 +0545

    feat: add parameters to sieve function.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from typing import Literal
 import sieve
 import os
 import subprocess
 import shutil
 
+
+AspectRatioOptions = Literal["1:1", "4:3", "3:4", "4:5", "5:4", "9:16"]
+
 metadata = sieve.Metadata(
     title="Long form video repurposing tool.",
     description="Given a video generate multiple shorts that are highlights of the video.",
     tags=["Video", "Audio"],
-    # image=sieve.Image(
-    #     path="logo.jpg"
-    # ),
-    # readme=open("README.md", "r").read(),
+    image=sieve.Image(
+        path="logo.jpg"
+    ),
+    readme=open("README.md", "r").read(),
 )
 
 @sieve.function(
@@ -22,13 +26,20 @@ metadata = sieve.Metadata(
 
 def longform_to_shorts(
     file: sieve.File,
+    transcript_analysis_prompt : str = "",
+    target_language: str = "",
+    autocrop_prompt: str = "person",
+    autocrop_negative_prompt: str = "",
+    min_scene_length: int = 0,
+    aspect_ratio: AspectRatioOptions = '9:16'
+
 ):
     #TODO: remove unnecessary setting.    
     #TODO: exception handling.
     transcript_analysis_settings = {
         "transcription_backend": "groq-whisper",
         "llm_backend": "gpt-4o-2024-08-06",
-        "prompt": "",
+        "prompt": transcript_analysis_prompt,
         "generate_summary": False,
         "generate_title": False,
         "generate_tags": False,
@@ -40,7 +51,7 @@ def longform_to_shorts(
         "max_summary_length": 5,
         "max_title_length": 10,
         "num_tags": 5,
-        "target_language": "",
+        "target_language": target_language,
         "speaker_diarization": False,
         "use_vad": False,
         "denoise_audio": False,
@@ -52,7 +63,7 @@ def longform_to_shorts(
 
     autocrop_settings = {
         "active_speaker_detection": True,
-        "aspect_ratio": "9:16",
+        "aspect_ratio": aspect_ratio,
         "return_video": True,
         "start_time": 0,
         "end_time": -1,
@@ -61,13 +72,13 @@ def longform_to_shorts(
         "visualize": False,
         "include_subjects": False,
         "include_non_active_layouts": False,
-        "prompt": "person",
-        "negative_prompt": "",
+        "prompt": autocrop_prompt,
+        "negative_prompt": autocrop_negative_prompt,
         "single_crop_only": False,
         "crop_movement_speed": 0.1,
         "crop_sampling_interval": 3,
         "return_scene_data": False,
-        "min_scene_length": 0,
+        "min_scene_length": min_scene_length,
     }
 
     transcript_analysis = sieve.function.get("sieve/transcript-analysis")
@@ -93,21 +104,20 @@ def longform_to_shorts(
         end_time = highlight['end_time']
         output_file = f"{output_dir}/{title}_highlight.mp4"   
 
-        #TODO: encoding
         ffmpeg_command = [
             "ffmpeg",
-            "-y",                            # Allow overwriting
-            "-i", file.path,                 # Input video
-            "-ss", str(start_time),          # Start time
-            "-to", str(end_time),            # End time
+            "-y",                            
+            "-i", file.path,                 # Original video
+            "-ss", str(start_time),          
+            "-to", str(end_time),            
             "-c", "copy",                    # Copy streams without re-encoding
-            output_file                      # Output file
+            output_file                      
         ]
         try:
             subprocess.run(ffmpeg_command, check=True)
             print(f"Successfully created clip: {output_file}")
         except subprocess.CalledProcessError as e:
-            print(f"Error occurred while processing {title}.")
+            print(f"Error occurred while processing {title} with start at {str(start_time)} and end at {str(end_time)}.")
             raise e
         
         autocrop = sieve.function.get("sieve/autocrop")
